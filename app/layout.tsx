712a5d6fb1b20e77c064dee8a027ebd7fba3b1d8
joblagii-cmd <joblagii@gmail.com>
2026-06-06 14:10:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head><meta name="google-site-verification" content="mhwuIuZvhxIUI-g2YK2o1tfhIy8tt1J2VaZ3Yr9PwOo" /></head>
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
