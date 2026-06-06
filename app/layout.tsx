cc8edc912e5e283c30403e34aa4bc2bfe2ce8f5e
joblagii-cmd <joblagii@gmail.com>
2026-06-07 01:04:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head><meta name="google-site-verification" content="mhwuIuZvhxIUI-g2YK2o1tfhIy8tt1J2VaZ3Yr9PwOo" /></head>
+      <meta name="google-site-verification" content="9jFgTVeWd6lIyuth91m1WDwx62v47MbqrZ03lndiwzc" />
+<meta name="google-site-verification" content="0odExRPDqu8uvUfBMHh6xaNPWeAiqdqLsTcA8blKBU4" />
+<meta name="google-site-verification" content="QBhMTqn2elKzsIUrlhmEsMBxNMXLnrQI6AJKqQ2jDhc" />
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
