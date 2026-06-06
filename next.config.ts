e5363fd431d36fecc40d55c8441d45af84f7978f
joblagii-cmd <joblagii@gmail.com>
2026-06-06 13:14:02 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Allow writing to data directory in production
   experimental: {},
+  output: "standalone",
 };
 
 export default nextConfig;
